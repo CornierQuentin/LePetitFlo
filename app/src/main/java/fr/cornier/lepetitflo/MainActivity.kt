dde2a0605b3e5f2c6f98597001dd3a7fe9c9dd3d
Quentin Cornier <quentincornier95@gmail.com>
2020-04-28 19:14:08 +0200

    Création des 4 pions dans leurs positions initiales, création d'une fonction rattacher au clique du bouton "Lancer le dé" permettant de déplacer le pion 1 vers la case 21

--- app/src/main/java/fr/cornier/lepetitflo/MainActivity.kt
+++ app/src/main/java/fr/cornier/lepetitflo/MainActivity.kt
@@ -2,11 +2,26 @@ package fr.cornier.lepetitflo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintSet
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val set = ConstraintSet()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun test (button: View) {
+        val result = (1..24).random()
+
+        set.clone(constraintLayout)
+        set.connect(player1.id, ConstraintSet.TOP, case21.id, ConstraintSet.TOP, 25)
+        set.connect(player1.id, ConstraintSet.START, case21.id, ConstraintSet.START, 20)
+        set.applyTo(constraintLayout)
+    }
+
 }
