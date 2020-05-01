ce54538f5111f3a0a3193fa2fbe2f2e0e8cde828
Quentin Cornier <quentincornier95@gmail.com>
2020-05-01 15:39:36 +0200

    Mise en fonction de certaine partie du code. Documentation de la plupart des fonctions. Corrections mineures

--- app/src/main/java/fr/cornier/lepetitflo/MainActivity.kt
+++ app/src/main/java/fr/cornier/lepetitflo/MainActivity.kt
@@ -2,6 +2,7 @@ package fr.cornier.lepetitflo
 
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintSet
@@ -11,8 +12,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     var testMalusCase = 0
-
-    var playerNumber = 4
 
     private var diceResult = 0
 
@@ -25,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     var playerFinishTest = arrayOf(0, 0, 0, 0)
 
-    var textList = arrayOf("Case\nDépart", "40\nFentes\nSautées", "30\nBurpees", "2 min\nMountain\nClimber", "40\nSquats Sautés", "2 min\nGainage\nPendule", "30\nFrog\nJumps", "100\nCrunchs", "3 min\nPapillion", "Reculer\nDe\n2 Cases", "2 min\nGainage", "BSU\nSquat", "30\nPompes", "2 min\nChaise", "Repos", "40\nDeeps", "BSU\nAbdos", "30\nFrog\nJumps", "40\nFentes\nSautées", "Retour à \nla Case\nDépart", "2 min\nCrunch\nBicylcle", "50\nSquats", "100\nCrunchs", "Reculer\nDe\n5 Cases", "4 min\nGainage", "Fin")
+    var textList = arrayOf("Case\nDépart", "40\nFentes\nSautées", "30\nBurpees", "2 min\nMountain\nClimber", "40\nSquats\nSautés", "2 min\nGainage\nPendule", "30\nFrog\nJumps", "100\nCrunchs", "3 min\nPapillion", "Reculer\nDe\n2 Cases", "2 min\nGainage", "BSU\nSquat", "30\nPompes", "2 min\nChaise", "Repos", "40\nDeeps", "BSU\nAbdos", "30\nFrog\nJumps", "40\nFentes\nSautées", "Retour à \nla Case\nDépart", "2 min\nCrunch\nBicylcle", "50\nSquats", "100\nCrunchs", "Reculer\nDe\n5 Cases", "4 min\nGainage", "Fin")
 
     var buttonList = arrayListOf(R.drawable.blue_button, R.drawable.green_button, R.drawable.yellow_button, R.drawable.red_button)
 
@@ -42,6 +41,13 @@ class MainActivity : AppCompatActivity() {
 
 
     fun onDiceButtonClick(button: View) {
+        /*
+        args : Prend en argument le bouton cliquer
+
+        Choisi un nombre aléatoire entre 1 et 6. pour ensuite l'envoyer dans la fonction displayDiceResult().
+        Et après 1000 ms passe à la fonction gameTurn()
+         */
+
         diceResult = (1..6).random()
 
         displayDiceResult(diceResult)
@@ -56,19 +62,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun gameTurn() {
+        /*
+        No arguments*
+
+        Effectue l'entièreté du tour d'un joueur.
+        Il vérifier que le joueur n'as pas déjà fini, si c'est le cas il passe au joueur d'après.
+        Il remet la couleur de bas de la case ou le joueur se trouve
+        Il augmente le score du joueur.
+        Il déplace le joueur jusqu'à sa case ne vérifiant si il n'est pas sur une case malus,
+        si c'est le cas il applique le malus et le déplace sur sa case final.
+        Il Affiche la case sur laquelle le joueur est tombé, si il n'était pas tomber sur une case malus.
+        Il change la couleur de la case sur laquelle vient d'arriver le joueur.
+        Il vérifie que le joueur n'est pas fini, si c'est le cas il passe sa valeur à 1 dans la variable playerFinishTest.
+        Il passe augmente le playerTurn.
+        Il vérifie que les 4 joueurs n'ont pas fini.
+        Il change la couleur du bouton par rapport au prochain joueur devant jouer.
+         */
+
         var testCaseClear: Int
 
         testMalusCase = 0
 
-        if (playerFinishTest[playerTurn] == 1) {
-            while (playerFinishTest[playerTurn] == 1) {
-                if (playerTurn in 0..2) {
-                    playerTurn += 1
-                } else {
-                    playerTurn = 0
-                }
-            }
-        }
+        isPlayerAlreadyFinished()
+
         if (playerScoreList[playerTurn] != 0 && playerScoreList[playerTurn] != 25) {
             caseList[playerScoreList[playerTurn]].setBackgroundResource(R.drawable.normal_case)
         }
@@ -90,7 +106,7 @@ class MainActivity : AppCompatActivity() {
 
                 playerScoreList[playerTurn] = playerScoreList[playerTurn] - 2
 
-                testCaseClear = isCaseClear()
+                testCaseClear = isCaseClear(playerScoreList[playerTurn])
 
                 movePlayer(playerTurn, testCaseClear)
             }
@@ -101,7 +117,7 @@ class MainActivity : AppCompatActivity() {
 
                 playerScoreList[playerTurn] = 0
 
-                testCaseClear = isCaseClear()
+                testCaseClear = isCaseClear(playerScoreList[playerTurn])
 
                 movePlayer(playerTurn, testCaseClear)
             }
@@ -112,13 +128,13 @@ class MainActivity : AppCompatActivity() {
 
                 playerScoreList[playerTurn] = playerScoreList[playerTurn] - 5
 
-                testCaseClear = isCaseClear()
+                testCaseClear = isCaseClear(playerScoreList[playerTurn])
 
                 movePlayer(playerTurn, testCaseClear)
             }
             else -> {
-                testCaseClear = isCaseClear()
-                
+                testCaseClear = isCaseClear(playerScoreList[playerTurn])
+
                 movePlayer(playerTurn, testCaseClear)
             }
         }
@@ -133,32 +149,23 @@ class MainActivity : AppCompatActivity() {
 
         if (playerScoreList[playerTurn] >= 25) {
             playerFinishTest[playerTurn] = 1
-            playerNumber -= 1
         }
 
-        if (playerTurn in 0..2) {
-            playerTurn += 1
-        }else {
-            playerTurn = 0
-        }
+        nextPlayerTurn()
 
-        if (playerFinishTest[0] == 1 && playerFinishTest[1] == 1 && playerFinishTest[2] == 1 && playerFinishTest[3] == 1) {
-            dé.visibility = View.INVISIBLE
-            continuer.visibility = View.GONE
+        isFinish()
 
-            blurLayout.visibility = View.VISIBLE
-            restart.visibility = View.VISIBLE
-
-            textAffichage.text = "Fini, Bravo !!"
-
-            textAffichage.visibility = View.VISIBLE
-            caseAffichage.visibility = View.VISIBLE
-        }
-
-        changeButtonColor(playerTurn)
+        changeButtonColor()
     }
 
     private fun movePlayer(playerTurn:Int, testCaseClear:Int) {
+        /*
+        args : prend en arguments le playerTurn et le compteur du nombre de joueur sur la case
+
+        Déplace le joueur en modifiant ses parents pour changer ses contraintes, tout en changeant son écart avec le haut
+        suivant le nombre de joueur déjà présent sur la case.
+         */
+
         when (testCaseClear) {
             1 -> {
                 set.clone(constraintLayout)
@@ -236,6 +243,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun afficherCase(playerScore:Int) {
+        /*
+        args : prend en argument le score du joueur
+
+        Affiche la case sur correspondant au score du joueur.
+         */
+
         dé.visibility = View.GONE
 
         blurLayout.visibility = View.VISIBLE
@@ -249,6 +262,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun afficherCaseMalus(playerScore:Int) {
+        /*
+        args : prend en arguments le score du joueur
+
+        Affiche la case malus, correspondant au score du joueur, pendant 2000 ms pour ensuite utiliser
+        la fonction displayCaseAfterMalus().
+         */
+
         dé.visibility = View.GONE
 
         blurLayout.visibility = View.VISIBLE
@@ -268,10 +288,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onRestartButtonClick(button: View) {
+        /*
+        args : prend en argument le bouton cliquer
+
+        Redémarre l'activité
+         */
+
         recreate()
     }
 
     fun onContinueButtonClick(button: View) {
+        /*
+        args : prend en argument le bouton cliquer
+
+        Cache l'affichage de la case sur laquelle est tombé le joueur
+         */
+
         dé.visibility = View.VISIBLE
 
         blurLayout.visibility = View.INVISIBLE
@@ -282,6 +314,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun displayCaseAfterMalus() {
+        /*
+        No arguments
+
+        Affiche la case sur laquelle tombe un joueur après avoir reçu un malus
+         */
+
         var playerPlay = playerTurn
 
         continuer.visibility = View.VISIBLE
@@ -305,6 +343,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun displayDiceResult (diceResult:Int) {
+        /*
+        args : prend en arguments le résultat du dé
+
+        Affiche le résultat du dé
+         */
+
         dé.visibility = View.GONE
 
         diceResultText.text = "$diceResult"
@@ -314,6 +358,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hideDiceResult () {
+        /*
+        No arguments
+
+        Cache l'affichage du résultat du dé
+         */
+
         dé.visibility = View.VISIBLE
 
         diceResultText.visibility = View.INVISIBLE
@@ -321,26 +371,90 @@ class MainActivity : AppCompatActivity() {
         blurLayout.visibility = View.INVISIBLE
     }
 
-    private fun changeButtonColor(playerTurn:Int) {
+    private fun changeButtonColor() {
+        /*
+        No arguments
+
+        Change la couleur du bouton pour celle du prochain joueur devant jouer
+         */
+
+        isPlayerAlreadyFinished()
         dé.setBackgroundResource(buttonList[playerTurn])
     }
 
-    private fun isCaseClear() : Int {
+    private fun isCaseClear(case: Int) : Int {
+        /*
+        args : prend en argument la case sur laquelle il faut faire le test.
+
+        La variable tesCaseClear est un compteur permettant dee savoir le nombre de personne sur une case.
+        Elle augmente de 1 chaque fois que le score d'un joueur est égal au numéro de la case.
+         */
+
         var testCaseClear = 0
 
-        if (playerScoreList[playerTurn] == playerScoreList[0]) {
+        if (case == playerScoreList[0]) {
             testCaseClear += 1
         }
-        if (playerScoreList[playerTurn] == playerScoreList[1]) {
+        if (case == playerScoreList[1]) {
             testCaseClear += 1
         }
-        if (playerScoreList[playerTurn] == playerScoreList[2]) {
+        if (case == playerScoreList[2]) {
             testCaseClear += 1
         }
-        if (playerScoreList[playerTurn] == playerScoreList[3]) {
+        if (case == playerScoreList[3]) {
             testCaseClear += 1
         }
 
         return testCaseClear
+    }
+
+    private fun isFinish() {
+        /*
+        No arguments
+
+        Quand les 4 joueurs sont arrivés à la fin, un message s'affiche pour les féliciter et un bouton apparait pour recommencer.
+         */
+
+        if (playerFinishTest[0] == 1 && playerFinishTest[1] == 1 && playerFinishTest[2] == 1 && playerFinishTest[3] == 1) {
+            dé.visibility = View.INVISIBLE
+            continuer.visibility = View.GONE
+
+            blurLayout.visibility = View.VISIBLE
+            restart.visibility = View.VISIBLE
+
+            textAffichage.text = "Fini, Bravo !!"
+
+            textAffichage.visibility = View.VISIBLE
+            caseAffichage.visibility = View.VISIBLE
+        }
+    }
+
+    private fun nextPlayerTurn () {
+        /*
+        No arguments
+
+        Permet de passer au playerTurn suivant. Si le playerTurn est entre 0 et 2 alors il est augmenté de 1,
+        sinon s'il est égal à 3 il est redescendu à 0.
+         */
+
+        if (playerTurn in 0..2) {
+            playerTurn += 1
+        }else {
+            playerTurn = 0
+        }
+    }
+
+    private fun isPlayerAlreadyFinished() {
+        /*
+        No arguments
+
+        Si le joueur du playerTurn à déjà fini alors le playerTurn est augmenté ou remis à 0 jusqu'à ce que le joueur du playerTurn ai fini.
+         */
+
+        if (playerFinishTest[playerTurn] == 1) {
+            while (playerFinishTest[playerTurn] == 1) {
+                nextPlayerTurn()
+            }
+        }
     }
 }
